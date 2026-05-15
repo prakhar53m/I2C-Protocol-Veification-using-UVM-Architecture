a3ec5e282f2a3a9db5d2265e7f4b757b7f423d25
Prakhar Mall <prakharmall1999@gmail.com>
2026-05-15 18:19:44 +0530

    Write agent config

---
+++ write_agents/i2c_wr_agent_cfg.sv
@@ -0,0 +1,6 @@
+class i2c_wr_agent_cfg extends uvm_object;
+    `uvm_object_utils(i2c_wr_agent_cfg)
+    uvm_active_passive_enum is_active = UVM_ACTIVE;
+    virtual i2c_if vif;
+    function new(string name="i2c_wr_agent_cfg"); super.new(name); endfunction
+endclass
